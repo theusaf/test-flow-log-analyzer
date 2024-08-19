00fcf7e151efc8bca30e4fb90551cb214a76d69a
theusaf <theusafyt@gmail.com>
2024-08-19 13:14:09 -0700

    test(parse_lookup_table): verify creation of recoord objects

--- test/test_parse_lookup_table.py
+++ test/test_parse_lookup_table.py
@@ -1,5 +1,6 @@
 import unittest
 from src.parse_lookup_table import parse_lookup_table
+from src.log_record import LogRecord
 
 
 class TestParseLookupTable(unittest.TestCase):
@@ -7,6 +8,17 @@ class TestParseLookupTable(unittest.TestCase):
         table = "dstport,protocol,tag"
         result = parse_lookup_table(table)
         self.assertIsInstance(result, list)
+
+    def test_returns_list_of_log_records(self):
+        table = """
+dstport,protocol,tag
+25,tcp,sv_P1
+68,udp,sv_P2
+""".strip()
+        result = parse_lookup_table(table)
+        self.assertEqual(len(result), 2)
+        for record in result:
+            self.assertIsInstance(record, LogRecord)
 
 
 if __name__ == "__main__":
