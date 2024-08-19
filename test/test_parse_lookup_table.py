e68cacfd27c063d2502ea5edf911de51bd89872f
theusaf <theusafyt@gmail.com>
2024-08-19 14:40:24 -0700

    test(parse_lookup_table): verify capitalization of protocl is mapped accurately

--- test/test_parse_lookup_table.py
+++ test/test_parse_lookup_table.py
@@ -26,7 +26,20 @@ dstport,protocol,tag
 25,tcp,sv_P1
 68,udp,sv_P2
 """.strip()
-        # Note: The values here are not accurate
+        result = parse_lookup_table(table)
+        self.assertEqual(result[0].fields["dstport"], "25")
+        self.assertEqual(result[0].fields["protocol"], "6")
+        self.assertEqual(result[0].tag, "sv_P1")
+        self.assertEqual(result[1].fields["dstport"], "68")
+        self.assertEqual(result[1].fields["protocol"], "17")
+        self.assertEqual(result[1].tag, "sv_P2")
+
+    def test_fields_are_case_insensitive(self):
+        table = """
+dstport,protocol,tag
+25,TCP,sv_P1
+68,UDP,sv_P2
+""".strip()
         result = parse_lookup_table(table)
         self.assertEqual(result[0].fields["dstport"], "25")
         self.assertEqual(result[0].fields["protocol"], "6")
